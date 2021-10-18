de8a62ac9885b80d8d20fc7b97b3a13930725ddf
Ushio Shugo <ushio.s@gmail.com>
2021-10-18 09:43:55 +0900

    Add check token method

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import urllib
+import boto3
 from warrant.aws_srp import AWSSRP
+from warrant import Cognito
 from requests import Request, Session, Response
 
 class Client:
@@ -7,38 +9,42 @@ class Client:
     user_pool_id = None
     client_id = None
 
+    __region_name = None
+
     __id_token = None
     __refresh_token = None
     __access_token = None
 
     __base_url = None
-    __session = None
+    __requests_session = None
     __timeout = None
 
 
     def __init__(
         self,
-        client,
+        session: boto3.Session,
         base_url: str,
         user_pool_id: str,
         client_id: str,
         * ,
-        session = None,
-        timeout = 5,
+        timeout: int = 5,
+        requests_session: Session = None,
     ):
-        self.client = client
+        self.client = session.client('cognito-idp')
+        self.__region_name = session.region_name
+        self.__requests_session = requests_session or Session()
         self.user_pool_id = user_pool_id
         self.client_id = client_id
-        self.__session = session or Session()
         self.__base_url = base_url
         self.__timeout = timeout
+        
     
     @property
     def id_token(self):
         return self.__id_token
 
     def signin(self, username: str, password: str):
-        aws = AWSSRP( # docs: https://github.com/capless/warrant
+        aws = AWSSRP( # https://github.com/capless/warrant#using-awssrp
             username=username,
             password=password,
             pool_id=self.user_pool_id,
@@ -55,8 +61,22 @@ class Client:
             self.__access_token = result.get("AccessToken", None)
 
         return tokens
+
+    @property
+    def __cognito(self) -> Cognito:
+        return Cognito( 
+            self.user_pool_id,
+            self.client_id,
+            id_token=self.__id_token,
+            refresh_token=self.__refresh_token,
+            access_token=self.__access_token,
+            user_pool_region=self.__region_name,
+        )
+    
+    def is_token_expired(self):
+        return self.__cognito.check_token() # https://github.com/capless/warrant#check-token 
     
     def do(self, req: Request) -> Response:
         req.headers['Authorization'] = self.__id_token
         req.url = urllib.parse.urljoin(self.__base_url, req.url)
-        return self.__session.send(req.prepare(), timeout=self.__timeout)
+        return self.__requests_session.send(req.prepare(), timeout=self.__timeout)
--- main.py
+++ main.py
@@ -12,14 +12,9 @@ import client
 dotenv_path = path.join(path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-idp_client = boto3.client(
-    'cognito-idp',
-    region_name = "ap-northeast-1",
-)
-
 def main():
     c = client.Client(
-        client=idp_client,
+        session=boto3.Session(region_name="ap-northeast-1"),
         base_url=os.getenv("ORION_ENDPOINT"),
         user_pool_id=os.getenv("USER_POOL_ID"),
         client_id=os.getenv("APP_CLIENT_ID"),
@@ -30,11 +25,13 @@ def main():
         password=os.getenv("PASSWORD")
     )
 
-    # print(f"idToken: {c.id_token}")
+    print(f"idToken: {c.id_token}")
 
     resp = c.do(requests.Request('GET', "/version"))
     j = json.loads(resp.content)
     print(json.dumps(j, indent=2))
+
+    print("expired:", c.is_token_expired())
     
     
 if __name__ == '__main__':
